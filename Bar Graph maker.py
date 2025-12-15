984b98c253b85931f32868f13da2d9f8b1ea6316
venkataramavinaybandla-crypto <venkataramavinaybandla@gmail.com>
2025-12-15 13:30:50 +0530

    Graph maker.py

--- Bar Graph maker.py
+++ Bar Graph maker.py
@@ -1,14 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-Categories = np.array(["Grains", "Fruit", "Vegetables", "Protein", "Dairy", "Sweets"])
-values = np.array([6, 8, 9, 7, 4, 2])
+Categories = np.array(["Grains", "Fruit", "Veget-\nables", "Protein", "Dairy", "Sweets"])
+values = np.array([689, 891, 908, 741, 496, 231])
 
-plt.bar(Categories, values, color="skyblue")
+choice = input("(Bar Graph / Pie Chart): ")
 
-plt.title("Categories")
-plt.xlabel("Consuming Resources")
-plt.ylabel("Value")
-plt.xticks(rotation=0)
+if choice == "Bar Graph":
+    plt.bar(Categories, values, color="green")
+    plt.title("Categories")
+    plt.xlabel("Consuming Resources")
+    plt.ylabel("Value")
+    plt.xticks(rotation=0)
+    plt.show()
 
-plt.show()
+elif choice == "Pie Chart":
+    plt.pie(values, labels=Categories, autopct="%1.1f%%")
+    plt.title("Categories")
+    plt.show()
+
+else:
+    print("Invalid choice! Please type Bar Graph or Pie Chart.")
